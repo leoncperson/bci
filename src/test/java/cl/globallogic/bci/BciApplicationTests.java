3ea6d75ae25451e4ac5feda894536886d223907e
cristian <cbenez@gmail.com>
2022-09-03 08:49:45 -0300

    comit inicial

---
+++ src/test/java/cl/globallogic/bci/BciApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.globallogic.bci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
